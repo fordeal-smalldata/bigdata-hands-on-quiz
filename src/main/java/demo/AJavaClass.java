e7cc96cf551b0eb0d2342c3088ff1975622bc301
Renkai <sungan@fordeal.com>
2019-09-04 16:31:33 +0800

    add call java demo

--- src/main/java/demo/AJavaClass.java
+++ src/main/java/demo/AJavaClass.java
@@ -1,7 +1,20 @@
 package demo;
 
+
+import java.util.Arrays;
+import java.util.List;
+
 public class AJavaClass {
     public void aJavaMethod() {
         System.out.println("这是一个Java方法");
+    }
+
+    public List<Integer> getJavaList() {
+        return Arrays.asList(1, 0, 2, 4);
+    }
+
+    public void printList(List<Integer> list) {
+        System.out.println("数组中包含元素:");
+        list.forEach(System.out::println);
     }
 }
--- src/main/scala/demo/ScalaCallJava.scala
+++ src/main/scala/demo/ScalaCallJava.scala
@@ -2,7 +2,18 @@ package demo
 
 object ScalaCallJava {
   def main(args: Array[String]): Unit = {
-    (new AJavaClass).aJavaMethod()
+    val jClass = new AJavaClass
+    jClass.aJavaMethod()
     //输出 这是一个Java方法
+
+    import scala.collection.JavaConverters._
+    val list = jClass.getJavaList.asScala
+    println("转换成Scala的标准库: " + list) // 输出 转换成Scala的标准库: Buffer(1, 0, 2, 4)
+    jClass.printList(list.asJava)
+    //输出 数组中包含元素:
+    //1
+    //0
+    //2
+    //4
   }
 }
