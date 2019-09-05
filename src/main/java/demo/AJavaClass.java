36b23f857a9afce82bc8595819fca7f6a42dc75d
Renkai <sungan@fordeal.com>
2019-09-05 14:31:08 +0800

    add call java demo

--- src/main/java/demo/AJavaClass.java
+++ src/main/java/demo/AJavaClass.java
@@ -17,4 +17,25 @@ public class AJavaClass {
         System.out.println("数组中包含元素:");
         list.forEach(System.out::println);
     }
+
+    static int maxEmployeeId = 9999;
+
+    public static void sendRedPack(int currentEmployeeId, int price) {
+        int recipientId = (currentEmployeeId + 1) % (maxEmployeeId + 1);
+        System.out.println("员工 " + currentEmployeeId + "发利是给员工" + recipientId + " " + price + " 元");
+        if (currentEmployeeId > 0) sendRedPack(currentEmployeeId - 1, price + 1);
+    }
+
+    public static void main(String[] args) {
+        sendRedPack(maxEmployeeId, maxEmployeeId);
+    }
+    //员工 9999发利是给员工0 9999 元
+    //员工 9998发利是给员工9999 10000 元
+    //员工 9997发利是给员工9998 10001 元
+    //员工 9996发利是给员工9997 10002 元
+    //员工 9995发利是给员工9996 10003 元
+    // .....
+    //Exception in thread "main" java.lang.StackOverflowError
+    //	at demo.AJavaClass.sendRedPack(AJavaClass.java:26)
+
 }
